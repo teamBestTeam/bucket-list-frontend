0fc52f4e1e58b431f6c825a8a73324bd59e7ae26
smb2255 <smb2255@columbia.edu>
2018-02-14 15:14:30 -0500

    Event listener changePass Add an event listener for the changePass button that brings a user to the change pass form.
    @quidprocrow
    @briankeegan
    @smb2255

--- assets/scripts/fillers/events.js
+++ assets/scripts/fillers/events.js
@@ -2,6 +2,7 @@ const mainTemplate = require('../templates/main.handlebars')
 const signUpTemplate = require('../templates/signUp.handlebars')
 const signInTemplate = require('../templates/signIn.handlebars')
 const profileTemplate = require('../templates/profile.handlebars')
+const changePassTemplate = require('../templates/changePass.handlebars')
 
 const onPageLoad = function () {
   const mainHTML = mainTemplate()
@@ -17,6 +18,11 @@ const onSignInLoad = function () {
   $('#content').html(signInHTML)
 }
 
+const onChangePassLoad = function () {
+  const changePassHTML = changePassTemplate()
+  $('#content').html(changePassHTML)
+}
+
 const onProfileLoad = function () {
   const profileHTML = profileTemplate()
   $('#content').html(profileHTML)
@@ -25,6 +31,7 @@ const onProfileLoad = function () {
 const eventListeners = function () {
   $('#content').on('click', '#signIn', onSignInLoad)
   $('#content').on('click', '#signUp', onSignUpLoad)
+  $('#content').on('click', '#changePass', onChangePassLoad)
 }
 
 module.exports = {
