77ed87bf1372fc9d1a213b5f77db3d911e1317dd
briankeegan <brian.morris.keegan@gmail.com>
2018-02-15 16:02:31 -0500

    Create update item feature
    On edit button on item click card is replaced by update form
    On submission patch request is sent and on success onIndexItems runs,
    so items are updated.
    
    @quidprocrow
      @briankeegan
        @smb2255

--- assets/scripts/items/events.js
+++ assets/scripts/items/events.js
@@ -2,6 +2,8 @@ const getFormFields = require('../../../lib/get-form-fields.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
 
+const updateItemTemplate = require('../templates/updateItem.handlebars')
+
 const onCreateItemSubmit = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
@@ -19,8 +21,10 @@ const onIndexItems = function () {
 const onUpdateItemSubmit = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
+  console.log(data)
   api.updateItem(data)
     .then(ui.onUpdateItemSuccess)
+    .then(onIndexItems)
     .catch(ui.onUpdateItemFailure)
 }
 
@@ -32,11 +36,27 @@ const onDeleteItemClick = function () {
     .catch(ui.onDeleteItemFailure)
 }
 
+const onUpdateItemClick = function () {
+  const dataId = $(this).parents('.card').data('id')
+  const parent = $(this).parents('.card')
+  const title = parent.find('.card-title').text()
+  const description = parent.find('.card-description').text()
+  const item = {
+    _id: dataId,
+    title: title,
+    description: description
+  }
+  const updateItemHTML = updateItemTemplate({item: item})
+  $(this).parents('.card-body').html(updateItemHTML)
+}
+
 const userEventListeners = function () {
   $('#content').on('submit', '#createItemForm', onCreateItemSubmit)
   $('#content').on('click', '#indexItems', onIndexItems)
+  $('#content').on('submit', '.updateItemForm', onUpdateItemSubmit)
   $('#content').on('submit', '#updateItemForm', onUpdateItemSubmit)
   $('#content').on('click', '#deletItemButton', onDeleteItemClick)
+  $('#content').on('click', '#updateItemButton', onUpdateItemClick)
 }
 
 module.exports = {
