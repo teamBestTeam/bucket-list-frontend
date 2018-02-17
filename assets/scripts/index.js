9b03c3c54ad3be107b757d97f1f36de6046b0966
briankeegan <brian.morris.keegan@gmail.com>
2018-02-17 15:43:52 -0500

    Add twitter load function
    - Update functions to ES6 functionality
    - Create twitter.js
    - Add twitter tags to handlebars
    
    @quidprocrow
    @briankeegan

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,8 +5,25 @@ const config = require('./config')
 const fillers = require('./fillers/events')
 const users = require('./users/events')
 const items = require('./items/events')
+// const twitter = require('./twitter')
 
 $(() => {
+  // window.twttr = (function (d, s, id) {
+  //   const fjs = d.getElementsByTagName(s)[0]
+  //   const t = window.twttr || {}
+  //   if (d.getElementById(id)) return t
+  //   const js = d.createElement(s)
+  //   js.id = id
+  //   js.src = 'https://platform.twitter.com/widgets.js'
+  //   fjs.parentNode.insertBefore(js, fjs)
+  //
+  //   t._e = []
+  //   t.ready = function (f) {
+  //     t._e.push(f)
+  //   }
+  //   return t
+  // }(document, 'script', 'twitter-wjs'))
+
   setAPIOrigin(location, config)
   fillers.onPageLoad()
   fillers.eventListeners()
